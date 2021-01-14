9e30b2faf437822e190d6e670f0c7920dfd2bc11
sunghyunjeonme <4dallove@gmail.com>
2021-01-14 15:27:08 +0900

    Change github username

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  title: `Sounghyun Jeon`,
-  description: `Sounghyun Jeon의 블로그입니다. 프론트엔드 개발과 관련된 일련의 경험과 배운 것을 블로그에 기록합니다.`,
-  author: `hyuns619`,
+  title: `Sunghyun Jeon`,
+  description: `Sunghyun Jeon
+  의 블로그입니다. 프론트엔드 개발과 관련된 일련의 경험과 배운 것을 블로그에 기록합니다.`,
+  author: `hyun`,
   introduction: `개발을 진심으로 즐길 수 있는 개발자가 되기를 희망합니다. 블로그에서는 공부하는 일련의 과정을 기록하고 있습니다.`,
   siteUrl: `https://hyuns.netlify.app/`, // Your blog 
   sitemapPath: `https://hyuns.netlify.app/sitemap.xml`,
   robotsPolicy: [{ userAgent: '*', allow: '/' }],
   social: {
     twitter: ``, // Your Twitter account
-    github: `hyuns619`, // Your GitHub account
+    github: `sunghyunjeonme`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     instagram: `hyuns619`, // Your LinkedIn account
@@ -27,7 +28,7 @@ module.exports = {
   ],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'hyuns619/hyuns.netlify.app', // Your repository for archive comment
+    utterances: 'sunghyunjeonme/hyuns.netlify.app', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/hyuns619"
+      href="https://github.com/sunghyunjeonme"
       className="github"
       aria-label="GitHub"
       target="_blank"
